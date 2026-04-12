fc9673f984bd4375e76eeca138506e0862975bfd
Arad David <davidarad02@gmail.com>
2026-04-12 22:27:31 +0300

    Add padding space after INFO level for alignment

--- src/logging.c
+++ src/logging.c
@@ -15,6 +15,11 @@ static const char *get_level_color(const char *level) {
     return COLOR_RESET;
 }
 
+static const char *get_level_pad(const char *level) {
+    if (level[0] == 'I') return " ";
+    return "";
+}
+
 void log_message(const char *level, const char *file, int line, const char *msg, ...) {
     struct timeval tv;
     gettimeofday(&tv, NULL);
@@ -29,6 +34,7 @@ void log_message(const char *level, const char *file, int line, const char *msg,
     }
 
     const char *color = get_level_color(level);
+    const char *pad = get_level_pad(level);
 
     va_list args;
     va_start(args, msg);
@@ -37,7 +43,7 @@ void log_message(const char *level, const char *file, int line, const char *msg,
     printf("%s.%06ld ", timestamp, tv.tv_usec);
     printf(COLOR_RESET "[");
     printf(COLOR_BOLD "%s", color);
-    printf("%s", level);
+    printf("%s%s", level, pad);
     printf(COLOR_RESET "] ");
     vprintf(msg, args);
     printf(" [");
