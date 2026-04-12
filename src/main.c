4f0a9be5ac8ea281926cc5a1bccc75ee9d50b97b
Arad David <davidarad02@gmail.com>
2026-04-12 22:21:43 +0300

    Add example logging usage to main.c

--- src/main.c
+++ src/main.c
@@ -5,5 +5,10 @@ int main() {
     LOG_DEBUG("Debug message");
     LOG_TRACE("Trace message");
     LOG_INFO("Application exiting");
+
+    LOG_INFO("User %d logged in", 42);
+    LOG_DEBUG("Value is %d, pointer is %p", 100, (void*)0x1234);
+    LOG_TRACE("Processing item %s", "sword");
+
     return 0;
 }
