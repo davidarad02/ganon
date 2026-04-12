2f7095949b44c7401921f23c965fdb491f77ac89
Arad David <davidarad02@gmail.com>
2026-04-12 22:28:49 +0300

    Logging enabled only in debug builds using __DEBUG__ macro

--- include/logging.h
+++ include/logging.h
@@ -3,9 +3,15 @@
 
 #include <stdio.h>
 
+#ifdef __DEBUG__
 #define LOG_INFO(msg, ...)  log_message("INFO", __FILE__, __LINE__, msg, ##__VA_ARGS__)
 #define LOG_DEBUG(msg, ...) log_message("DEBUG", __FILE__, __LINE__, msg, ##__VA_ARGS__)
 #define LOG_TRACE(msg, ...) log_message("TRACE", __FILE__, __LINE__, msg, ##__VA_ARGS__)
+#else
+#define LOG_INFO(msg, ...)
+#define LOG_DEBUG(msg, ...)
+#define LOG_TRACE(msg, ...)
+#endif
 
 void log_message(const char *level, const char *file, int line, const char *msg, ...);
 
