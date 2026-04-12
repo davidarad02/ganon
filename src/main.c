bb4e78c6ed095d054a4c907c376ff829618da773
Arad David <davidarad02@gmail.com>
2026-04-12 22:48:53 +0300

    Add loop_demo function demonstrating BREAK_IF and CONTINUE_IF

--- src/main.c
+++ src/main.c
@@ -15,11 +15,29 @@ l_cleanup:
     return rc;
 }
 
+int loop_demo(int *arr, int count) {
+    int rc = 0;
+    int sum = 0;
+
+    for (int i = 0; i < count; i++) {
+        CONTINUE_IF(arr[i] < 0);
+        BREAK_IF(sum > 100);
+        sum += arr[i];
+    }
+
+l_cleanup:
+    return sum;
+}
+
 int main() {
     int result = 0;
 
     rc_demo(1, 2, &result);
     LOG_INFO("rc_demo returned %d", result);
+
+    int arr[] = {10, 20, -5, 30, 40};
+    int sum = loop_demo(arr, 5);
+    LOG_INFO("loop_demo sum: %d", sum);
 
     return 0;
 }
