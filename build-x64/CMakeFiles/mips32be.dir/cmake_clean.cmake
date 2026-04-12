db6b0eccbe9fff694e1bb55597490753a0418200
Arad David <davidarad02@gmail.com>
2026-04-12 21:49:28 +0300

    Setup cross-compilation: x64, armv5, mips32be with static linking

--- build-x64/CMakeFiles/mips32be.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/mips32be"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mips32be.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
