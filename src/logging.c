644c3f27851a572935fa7f7a9e780ba6fa54a702
Arad David <davidarad02@gmail.com>
2026-04-12 22:22:19 +0300

    Log: show only filename without path

--- src/logging.c
+++ src/logging.c
@@ -8,11 +8,16 @@ void log_message(const char *level, const char *file, int line, const char *msg,
     char timestamp[32];
     strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", tm_info);
 
+    const char *filename = file;
+    for (const char *p = file; *p; p++) {
+        if (*p == '/') filename = p + 1;
+    }
+
     va_list args;
     va_start(args, msg);
 
     printf("%s [%s] ", timestamp, level);
     vprintf(msg, args);
-    printf(" [%s:%d]\n", file, line);
+    printf(" [%s:%d]\n", filename, line);
     va_end(args);
 }
