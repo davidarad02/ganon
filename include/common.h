959ff63d162e6575c3fde651d2eb4708a0d10041
Arad David <davidarad02@gmail.com>
2026-04-12 23:00:48 +0300

    Fix BREAK_IF and CONTINUE_IF macros to work with outer loop

--- include/common.h
+++ include/common.h
@@ -9,9 +9,9 @@
     do { if (condition) { rc = error; goto l_cleanup; } } while (0)
 
 #define BREAK_IF(condition) \
-    do { if (condition) { break; } } while (0)
+    if (condition) { break; }
 
 #define CONTINUE_IF(condition) \
-    do { if (condition) { continue; } } while (0)
+    if (condition) { continue; }
 
 #endif
