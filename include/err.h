e65cee5579646558b44eec73e8e4f493a91d516a
Arad David <davidarad02@gmail.com>
2026-04-13 00:12:59 +0300

    Organize error codes by module with distinct ranges
    - main.c errors: 0x100-0x1FF
    - args.c errors: 0x200-0x2FF
    - Provides 256 error codes per module

--- include/err.h
+++ include/err.h
@@ -5,11 +5,11 @@ typedef enum {
     /* Success */
     E__SUCCESS = 0,
 
-    /* main.c */
-    E__MAIN__RC_DEMO__SOME_ERROR,
+    /* main.c (0x100) */
+    E__MAIN__RC_DEMO__SOME_ERROR = 0x100,
 
-    /* args.c */
-    E__ARGS__INVALID_ARGUMENTS,
+    /* args.c (0x200) */
+    E__ARGS__INVALID_ARGUMENTS = 0x200,
     E__ARGS__CONFLICTING_ARGUMENTS,
     E__ARGS__MISSING_VALUE,
     E__ARGS__INVALID_VALUE,
