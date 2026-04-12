b7658fbc075ed16d126e5328265ade3c0158b1f7
Arad David <davidarad02@gmail.com>
2026-04-12 22:47:56 +0300

    Add BREAK_IF and CONTINUE_IF macros to common.h

--- include/common.h
+++ include/common.h
@@ -6,4 +6,10 @@
 #define FAIL_IF(condition, error) \
     do { if (condition) { rc = error; goto l_cleanup; } } while (0)
 
+#define BREAK_IF(condition) \
+    do { if (condition) { break; } } while (0)
+
+#define CONTINUE_IF(condition) \
+    do { if (condition) { continue; } } while (0)
+
 #endif
